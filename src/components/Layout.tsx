60750bfd7d8d630f31748cac6f1af319ef3f531b
riccardocampani96 <riccardocampani96@gmail.com>
2026-05-28 16:37:22 +0200

    refactor(songs): update song metadata, normalize chords, and add song numbers  - letters B-C
    Aligned chord notations for consistency and readability, added missing song numbers (e.g., "Benedici, O Signore" and "Canzone della Speranza"), and applied minor formatting updates to lyrics.
    
    Signed-off-by: Riccardo Campani <riccardo.campani@alphagr.it>

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,22 +1,22 @@
-import { NavLink, Link, Outlet } from 'react-router-dom'
+import {Link, NavLink, Outlet} from 'react-router-dom'
 
 export function Layout() {
-  return (
-    <>
-      <header className="app-header">
-        <Link to="/" className="app-header__brand">
-          <h1>Libretto dei Canti</h1>
-        </Link>
-        <nav className="app-header__nav">
-          <NavLink to="/" end>
-            Messa di oggi
-          </NavLink>
-          <NavLink to="/canti">Indice</NavLink>
-        </nav>
-      </header>
-      <main>
-        <Outlet />
-      </main>
-    </>
-  )
+    return (
+        <>
+            <header className="app-header">
+                <Link to="/" className="app-header__brand">
+                    <h1>Libretto dei Canti</h1>
+                </Link>
+                <nav className="app-header__nav">
+                    <NavLink to="/" end>
+                        Messa di oggi
+                    </NavLink>
+                    <NavLink to="/canti">Indice</NavLink>
+                </nav>
+            </header>
+            <main>
+                <Outlet/>
+            </main>
+        </>
+    )
 }
